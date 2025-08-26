Managed to make SVI work, but converges to the wrong results!

diabayes/SVI.py
@@ -5,7 +5,7 @@ import jax
 import jax.numpy as jnp
 from jaxtyping import Array, Float
 
-from diabayes.typedefs import Variables
+from diabayes.typedefs import Variables, _Params
 
 ParticleArray = Float[Array, "N M"]
 GradientArray: TypeAlias = ParticleArray
@@ -83,19 +83,15 @@ def compute_phi(
 
 @eqx.filter_value_and_grad
 def _log_likelihood(
-    log_params: Float[Array, "..."],
+    log_params: _Params,
     mu_obs: Float[Array, "Nt"],
     noise_std: Float,
-    v0: Float,
-    forward_fn: Callable[[Float[Array, "2"], Float[Array, "..."]], Variables],
+    forward_fn: Callable[[_Params], Variables],
 ) -> Float:
     # Transform the log_params by taking exponential
-    params = jnp.exp(log_params)
-    # Get initial values
-    # TODO: replace with `get_steady_state` or something
-    y0 = jnp.array([mu_obs[0], params[2] / v0])
+    params = type(log_params).from_array(jnp.exp(log_params.to_array()))
     # Forward pass to get friction curve
-    mu_hat = forward_fn(y0, params).mu
+    mu_hat = forward_fn(params).mu
     # Log-likelihood of residuals
     p = -(jnp.square(mu_obs - mu_hat).mean() / (2 * noise_std**2))
     return p
@@ -107,5 +103,5 @@ decorated with eqx.filter_value_and_grad, it returns the log-
 likelihood and its gradient (hence it needs 2 values for the out_axes).
 """
 mapped_log_likelihood = jax.vmap(
-    _log_likelihood, in_axes=(0, None, None, None, None), out_axes=(0, 0)
+    _log_likelihood, in_axes=(0, None, None, None), out_axes=(0, 0)
 )

diabayes/solver.py
@@ -19,7 +19,6 @@ from diabayes.SVI import compute_phi, mapped_log_likelihood
 from diabayes.typedefs import (
     BayesianSolution,
     RSFParams,
-    RSFParticles,
     Variables,
     _BlockConstants,
     _Constants,
@@ -127,8 +126,8 @@ class ODESolver:
     @eqx.filter_jit
     def _forward_wrapper_SVI(
         self,
-        y0: Variables,
         params: _Params,
+        y0: Variables,
         t: Float[Array, "Nt"],
         friction_constants: _Constants,
         block_constants: _BlockConstants,
@@ -243,6 +242,7 @@ class ODESolver:
         t: Float[Array, "Nt"],
         mu: Float[Array, "Nt"],
         noise_std: Float,
+        y0: Variables,
         params: _Params,
         friction_constants: _Constants,
         block_constants: _BlockConstants,
@@ -264,14 +264,16 @@ class ODESolver:
 
         key, split_key = jr.split(key)
 
-        scale = jnp.ones(len(params)) * 0.1
+        Nparams = len(params.__dict__.keys())
+
+        scale = jnp.ones(Nparams) * 0.1
         inv_scale = 1 / (jnp.sqrt(2) * scale)
         log_params = jnp.log(params.to_array())
 
         # Sample particles from a log-normal distribution
-        log_particles = RSFParticles.generate(
+        log_particles = RSFParams.generate(
             N=Nparticles, loc=log_params, scale=scale, key=split_key
-        ).to_array()
+        )
 
         # Instantiate optimiser
         opt = optax.adam(learning_rate=self.learning_rate)
@@ -279,6 +281,7 @@ class ODESolver:
 
         forward_fn = partial(
             self._forward_wrapper_SVI,
+            y0=y0,
             t=t,
             friction_constants=friction_constants,
             block_constants=block_constants,
@@ -287,9 +290,7 @@ class ODESolver:
         @scan_tqdm(Nsteps)
         def body_fun(carry, i):
             params, state = carry
-            loss, gradp = mapped_log_likelihood(
-                params, mu, noise_std, friction_constants.v0, forward_fn
-            )
+            loss, gradp = mapped_log_likelihood(params, mu, noise_std, forward_fn)
             """
             Sometimes the adjoint back-propagation becomes unstable, 
             producing NaNs in the gradients. By setting the NaN-gradients
@@ -298,10 +299,14 @@ class ODESolver:
             likely be stable again and the particle will continue
             to be attracted by the maximum likelihood
             """
-            nan_count = jnp.isnan(gradp).sum() / gradp.shape[1]
-            gradp = jnp.asarray(jnp.where(jnp.isnan(gradp), 0.0, gradp))
-            gradq = -2 * (params - log_params) * inv_scale
-            phi = compute_phi(params, gradp, gradq)
+            params_array = params.to_array().T
+            gradp_array = gradp.to_array().T
+            nans = jnp.isnan(gradp_array)
+            nan_count = nans.sum() / nans.shape[1]
+            gradp_array = jnp.asarray(jnp.where(nans, 0.0, gradp_array))
+            gradq_array = -2 * (params_array - log_params) * inv_scale
+            phi_array = compute_phi(params_array, gradp_array, gradq_array)
+            phi = type(params).from_array(phi_array.T)
             updates, state = opt.update(phi, state, params)
             params = optax.apply_updates(params, updates)
             return (params, state), (loss.mean(), nan_count, params)

diabayes/typedefs.py
@@ -113,10 +113,48 @@ class Variables(eqx.Module):
         return cls(mu=mu, state=state_obj)
 
 
-class RSFParams(eqx.Module):
-    a: Float
-    b: Float
-    Dc: Float
+class Params(eqx.Module):
+
+    def __init__(self, *args, **kwargs):
+        field_names = [f.name for f in dcs.fields(self)]
+        for key, val in zip(field_names, args):
+            kwargs[key] = val
+
+        for key, val in kwargs.items():
+            object.__setattr__(self, key, jnp.asarray(val))
+
+        for name in field_names:
+            if not hasattr(self, name):
+                object.__setattr__(self, name, jnp.asarray(-1.0))
+
+    def __getitem__(self, idx):
+        x = dict((key.name, getattr(self, key.name)[idx]) for key in dcs.fields(self))
+        return type(self)(**x)
+
+    def __len__(self):
+        key = dcs.fields(self)[0].name
+        return len(getattr(self, key))
+
+    def to_array(self):
+        return jnp.squeeze(jnp.array(jax.tree_util.tree_flatten(self)[0]))
+
+    @classmethod
+    def from_array(cls, x):
+        return cls(*x)
+
+    @classmethod
+    def generate(
+        cls, N: int, loc: Float[Array, "M"], scale: Float[Array, "M"], key: jax.Array
+    ):
+        assert len(loc) == len(scale)
+        x = jr.normal(key, shape=(N, len(loc))) * scale + loc
+        return cls.from_array(x.T)
+
+
+class RSFParams(Params):
+    a: Float[Array, "..."]
+    b: Float[Array, "..."]
+    Dc: Float[Array, "..."]
 
 
 @dcs.dataclass(frozen=True)
@@ -125,7 +163,7 @@ class RSFConstants:
     mu0: Float
 
 
-class CNSParams(eqx.Module):
+class CNSParams(Params):
     phi_c: Float
     Z: Float
 
@@ -184,72 +222,6 @@ class StressTransfer(Protocol[BC]):
         constants: BC,
     ) -> Float: ...
 
-
-"""
---------------
-SVI containers
---------------
-"""
-
-
-@tree_util.register_pytree_node_class
-@dcs.dataclass(frozen=True)
-class Particles:
-
-    _particles: Union[Tuple[RSFParams, ...], Tuple[CNSParams, ...]]
-
-    def __getitem__(self, idx):
-        # Check if the requested selection is of the kind `x[1, 3]`
-        if isinstance(idx, tuple):
-            assert (
-                len(idx) == 2
-            ), "Particles do not support item selection with more than 2 indices"
-            i, j = idx
-            return self._particles[i].to_array()[j]
-        # Else the selection is of the kind `x[4]`
-        return self._particles[idx]
-
-    def __iter__(self):
-        return iter(self._particles)
-
-    def __len__(self):
-        return len(self._particles)
-
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        raise NotImplementedError
-
-    @classmethod
-    def from_array(cls, x: Iterable):
-        return cls.tree_unflatten(None, x)
-
-    def tree_flatten(self):
-        return self._particles, None
-
-    def to_array(self):
-        return jnp.array([p.tree_flatten()[0] for p in self._particles])
-
-    @classmethod
-    def generate(
-        cls, N: int, loc: Float[Array, "M"], scale: Float[Array, "M"], key: jax.Array
-    ):
-        assert len(loc) == len(scale)
-        x = jr.normal(key, shape=(N, len(loc))) * scale[None] + loc[None]
-        return cls.from_array(x)
-
-
-@tree_util.register_pytree_node_class
-@dcs.dataclass(frozen=True)
-class RSFParticles(Particles):
-
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        x = tuple(RSFParams(*child) for child in children)
-        return cls(x)
-
-
-# Alias gradients for semantic clarity
-Gradients: TypeAlias = Particles
 
 """
 -----------------
@@ -317,10 +289,11 @@ class BayesianSolution:
 
     def __init__(
         self,
-        chains: Chains,
+        x: Params,
         log_likelihood: Float[Array, "Nsteps"],
         nan_count: Float[Array, "Nsteps"],
     ):
+        chains = x.to_array().transpose(1, 2, 0)
         self.chains = jnp.exp(chains)
         self.final_state = jnp.exp(chains[-1])
         self.log_likelihood = log_likelihood

test/test_typedefs.py
@@ -26,33 +26,33 @@ class TestTypedefs:
         x2.pop("mu")
         assert tuple(x2.keys()) == variables3.state.keys
 
-    # def test_SVI_containers(self):
+    def test_SVI_containers(self):
 
-    #     import jax.random as jr
+        import jax.random as jr
 
-    #     key = jr.PRNGKey(42)
+        key = jr.PRNGKey(42)
 
-    #     # Test Particles
+        # Test Particles
 
-    #     key, split_key = jr.split(key)
-    #     Nparticles = 100
-    #     loc = jnp.array([1.0, 2.0, 3.0])
-    #     scale = jnp.ones(3)
-    #     particles = RSFParticles.generate(Nparticles, loc, scale, split_key)
+        key, split_key = jr.split(key)
+        Nparticles = 100
+        loc = jnp.array([1.0, 2.0, 3.0])
+        scale = jnp.ones(3)
+        particles = RSFParams.generate(Nparticles, loc, scale, split_key)
 
-    #     assert len(particles) == Nparticles
+        assert len(particles) == Nparticles
 
-    #     x = particles.to_array()
-    #     assert x.shape[1] == len(loc)
+        x = particles.to_array()
+        assert x.shape[0] == len(loc)
 
-    #     rtol = 2 / jnp.sqrt(Nparticles)
-    #     assert jnp.allclose(x.mean(axis=0), loc, rtol=rtol)
-    #     assert jnp.allclose(x.std(axis=0), scale, rtol=rtol)
+        rtol = 2 / jnp.sqrt(Nparticles)
+        assert jnp.allclose(x.mean(axis=1), loc, rtol=rtol)
+        assert jnp.allclose(x.std(axis=1), scale, rtol=rtol)
 
-    #     assert jnp.allclose(x, RSFParticles.from_array(x).to_array())
+        assert jnp.allclose(x, RSFParams.from_array(x).to_array())
 
-    #     # Test Chains
+        # Test Chains
 
-    #     pass
+        pass
 
     # def test_Bayesian_statistics(self): ...